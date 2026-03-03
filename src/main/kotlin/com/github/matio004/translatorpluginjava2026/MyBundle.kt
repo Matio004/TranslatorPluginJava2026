7d9028a60546fa4c673eec97ab4e6db8ebbdbc06
GitHub Action <action@github.com>
2026-03-03 16:12:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/matio004/translatorpluginjava2026/MyBundle.kt
+++ src/main/kotlin/com/github/matio004/translatorpluginjava2026/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.matio004.translatorpluginjava2026
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/matio004/translatorpluginjava2026/services/MyProjectService.kt
+++ src/main/kotlin/com/github/matio004/translatorpluginjava2026/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.matio004.translatorpluginjava2026.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.matio004.translatorpluginjava2026.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
